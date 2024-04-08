3cbe36bcc0c7fbe8b9a90510188563920e6ea38e
Sunho Ju <sunho5515@gmail.com>
2024-04-08 22:33:09 +0900

    레이아웃 세팅 완료

---
+++ src/components/Container.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Post />
+    <Post />
+    <Post />
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue'
+
+export default {
+    name: "layout-container",
+    components: {
+        Post,
+    }
+};
+</script>
+
+<style></style>
